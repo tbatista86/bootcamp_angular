161b986138c31feee612ca855776e58a1d5528a3
tbatista86 <tiagobatista86@gmail.com>
2020-02-19 07:46:06 -0300

    criando rota de info e criando coponent para editar curso selecionado

--- src/app/courses/courses-info.component.ts
+++ src/app/courses/courses-info.component.ts
@@ -1,5 +1,7 @@
+import { CourseService } from './course.service';
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from '@angular/router';
+import { Course } from './course';
 
 @Component({
     templateUrl: './courses-info.component.html'
@@ -7,11 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 
 export class CourseInfoComponent implements OnInit {
 
-    courseId: number;
+    course: Course;
 
-    constructor(private activatedRoute: ActivatedRoute) {  }
+    constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) {  }
 
     ngOnInit(): void {
-        this.courseId = +this.activatedRoute.snapshot.paramMap.get('id');
+        this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id'));
+    }
+
+    save(): void {
+        this.courseService.save(this.course);
     }
 }
